dc3389ad23c2b07247ceba66a42406a56a5e4ed1
Norhan Hamdy <norahamdy141@gmail.com>
2023-08-10 19:59:48 -0700

    tasks

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdin.h>
+/**
+ * main - the entry point 
+ * documentaion its a c program that print statment 
+ * return: Always 0 'sucess'
+ */
+int main(void)
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+		return(0);
+}
