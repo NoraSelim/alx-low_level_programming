0ee308d18034b3bc7d2e8fcf9e3098c966b6dc89
Norhan Hamdy <norahamdy141@gmail.com>
2023-08-11 03:02:39 -0700

    tasks

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
 * main - Entry point
 *
@@ -10,7 +11,7 @@ int main(void)
 {printf("Size of a char: 1 byte(s)
 Size of an int: 4 byte(s)
 Size of a long int: 4 byte(s)
-Size of a long long int: 8 byte(s(
-Size of a float: 4 byte(s)")
+Size of a long long int: 8 byte(s)
+Size of a float: 4 byte(s)");
 return (0);
 }
