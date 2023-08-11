e2f5ced0598083bf716705cb898e03f60a2f7861
Norhan Hamdy <norahamdy141@gmail.com>
2023-08-10 21:05:00 -0700

    tasks

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include<stdin.h>
 /**
- * main - the entry point 
- * documentaion its a c program that print statment 
- * return: Always 0 'sucess'
- */
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+*/
 int main(void)
-{
-
-	puts("\"Programming is like building a multilingual puzzle");
-		return(0);
+{puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
