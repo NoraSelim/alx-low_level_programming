d8c6d5a8cffb9945fdbe44bc34eb4b495d92f944
Norhan Hamdy <norahamdy141@gmail.com>
2023-08-11 01:52:19 -0700

    tasks

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 /**
-*main:entry point
-*Result:0 (success)
+* main:entry point
+*
+* Result:0 (success)
+*
 */
 int main(void)
 {printf("with proper grammar, but the outcome is a piece of art,\n");
