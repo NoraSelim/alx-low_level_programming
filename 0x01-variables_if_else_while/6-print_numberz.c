9bc4f8adcdd08662f08a7a1869d498b94c282868
Norhan Hamdy <norahamdy141@gmail.com>
2023-08-12 13:47:05 -0700

    tasks

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's about writinh alphabitics'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{int digit;
+for (digit = 48; digit <= 57; digit++)
+putchar(digit);
+putchar("\n");
+return (0);
+
