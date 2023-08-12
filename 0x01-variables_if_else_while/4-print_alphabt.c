4327a8cd7c33eb0ce4e801a319adb4af1fe52c2e
Norhan Hamdy <norahamdy141@gmail.com>
2023-08-12 09:45:22 -0700

    tasks

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdin.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's about writinh alphabitics'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{char ch, e, q;
+
+for (ch = 'a' ; ch <= 'z'; ch++)
+{
+if (ch != e && ch != q)
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
