077918bd6585e93b7979c5b01840bc24fbe2089f
Norhan Hamdy <norahamdy141@gmail.com>
2023-08-12 06:30:06 -0700

    tasks

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's about writinh alphabitics'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z'; ch++)
+	putchar(ch);
+
+	char UpCase;
+
+	for (UpCase = 'A'; UppCase <= 'Z'; UppCase++)
+	putchar(UpCase);
+	putchar("\n");
+	return (0);
+}
