626a1d1e5611ac14b6e9facaab1101a8cf0b5638
Norhan Hamdy <norahamdy141@gmail.com>
2023-08-12 14:25:30 -0700

    tasks

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's about writinh alphabitics'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{int digit;
+for (digit = 58; digit > 48 ; digit--)
+{putchar(digit);
+}
+putchar('\n');
+
+return (0);
+}
+
