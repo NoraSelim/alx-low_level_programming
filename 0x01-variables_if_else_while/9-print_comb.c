9a03ca4edbee9ba82822fa14caa2e13f3b76efc6
Norhan Hamdy <norahamdy141@gmail.com>
2023-08-14 02:40:34 -0700

    tasls

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,14 +8,15 @@
  */
 int main(void)
 {int digit;
-for (digit = 48; digit < 58; digit++)
-{putchar(digit);
+for (digit = 0; digit < 10; digit++)
+{putchar(digit + '0');
 
-if (digit < 57)
+if (digit < 9)
 
 putchar(',');
 putchar(' ');
 }
+putchar('\n');
 return (0);
 }
 
