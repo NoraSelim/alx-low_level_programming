ae4bb72806a31d4d4e7cbfdef5708b8ed0b9c0da
Norhan Hamdy <norahamdy141@gmail.com>
2023-08-15 15:58:50 -0700

    tasks

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Description: 'the program's about writinh alphabitics'
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int rows;
+	int ch;
+	for (row = 0 ;rows <=10 ;rows++)
+	{
+		for (ch = 'a' ; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+	}
+_putchar('\n');
+return (0);
+}
+
