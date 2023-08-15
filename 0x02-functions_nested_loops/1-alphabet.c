d9811b2e41e79a208755ef8996a00ff09308d292
Norhan Hamdy <norahamdy141@gmail.com>
2023-08-15 14:50:02 -0700

    tasks

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's about writinh alphabitics'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+for (ch = 'a' ; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
