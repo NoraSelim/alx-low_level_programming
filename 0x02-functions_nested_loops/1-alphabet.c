de4dbb5cbb2183372c022f03d85f85dfdb0c8303
Norhan Hamdy <norahamdy141@gmail.com>
2023-08-15 15:31:50 -0700

    tasks

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,15 @@
 #include "main.h"
 /**
- * main - Entry point
+ * print_alphabet - Entry point
  *
  * Description: 'the program's about writinh alphabitics'
  *
  * Return: Always 0 (Success)
  */
+
 void print_alphabet(void)
 {
-	int ch;
+int ch;
 for (ch = 'a' ; ch <= 'z'; ch++)
 {
 _putchar(ch);
