627a2f6f80f0dd259900e9efa8288a178da9d790
Norhan Hamdy <norahamdy141@gmail.com>
2023-08-15 18:35:07 -0700

    tasks

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _islower  - Entry point
+ *
+ * @c: 'the program's about writinh alphabitics'
+ *
+ * Return: Always 1 if lowercase otherwise return 0  (Success)
+ */
+
+int _islower(int c)
+{
+        if (c > 97 && c < 122 )
+        {
+                return (1);
+        }
+	
+	if (c > 65 && c < 90 )
+        {
+                return (1);
+        }
+return (0);
+}
+
+
