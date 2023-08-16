27652ccc6c922f891fbc200ab0e9d55dada3435f
Norhan Hamdy <norahamdy141@gmail.com>
2023-08-16 15:39:38 -0700

    tasks

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,15 +8,14 @@
  */
 void jack_bauer(void)
 {
-	int a;
-	int b;
+	int a, b;
 	for ( b = 0; b < 23 ; b++)
 	{
 		for ( a = 0; a < 59 ; a++)
 		{
 			_putchar((a / 10) + 48);
 			_putchar((a % 10) + 48);
-			_putchar(":");
+			_putchar(':');
 			_putchar((b / 10) + 48;
 			_putchar((b % 10) + 48);
 			_putchar('\n');
