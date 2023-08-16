7091f9fc5ec973612fcad9d1b7d4abab4cad27f4
Norhan Hamdy <norahamdy141@gmail.com>
2023-08-16 02:18:52 -0700

    tasks

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_sign  - Entry point
+ *
+ * @n: 'the program's about writinh alphabitics'
+ *
+ * Return: Always 1 if lowercase otherwise return 0  (Success)
+ */
+
+int print_sign(int n);
+{ if (n > 0)
+	{
+	_putchar('+');
+	_putchar(',');
+	return (1);
+	}
+		else if (n == 0)
+		{
+		_putchar('0');
+		_putchar(',');
+		return (0);
+		}
+		else (n < 0)
+		{
+		_putchar('-');
+		_putchar(',');
+		return (-1);
+		}
+}
