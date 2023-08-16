91c9c2a554e8c5ee7c31a31cb2d4dc1e9ecea4ae
Norhan Hamdy <norahamdy141@gmail.com>
2023-08-16 03:23:08 -0700

    tasks

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - Entry point
+ *
+ * @n: 'the program's about writinh alphabitics'
+ *
+ * Return: Always 1 if lowercase otherwise return 0  (Success)
+ */
+
+int _abs(int n)
+{
+if (n < 0)
+{
+	n = ( -1 * n);
+	return (n);
+}
+}
+
