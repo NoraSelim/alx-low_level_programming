08c54cd86bb6e38c2f507eb42c4c916b9f11ce41
Norhan Hamdy <norahamdy141@gmail.com>
2023-08-16 15:33:56 -0700

    tasks

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * jack_bauer - Entry point
+ *
+ * Description: 'the program's about time'
+ *
+ * Return: Always 0 (Success)
+ */
+void jack_bauer(void)
+{
+	int a;
+	int b;
+	for ( b = 0; b < 23 ; b++)
+	{
+		for ( a = 0; a < 59 ; a++)
+		{
+			_putchar((a / 10) + 48);
+			_putchar((a % 10) + 48);
+			_putchar(":");
+			_putchar((b / 10) + 48;
+			_putchar((b % 10) + 48);
+			_putchar('\n');
+		}
+	}
+}
+
