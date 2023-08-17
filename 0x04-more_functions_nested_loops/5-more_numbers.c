c240ce6f7b08fdfd8595350c7d00daf945e41925
Norhan Hamdy <norahamdy141@gmail.com>
2023-08-17 13:51:22 -0700

    tasks

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * more_numbers - Entry point
+ *
+ * Description: 'the program's about nested loops'
+ *
+ * Return: Always 0 (Success)
+ */
+void more_numbers(void)
+{
+	int lines = 0;
+
+	while (lines <= 10)
+	{
+		int n = 0;
+
+		while (n <= 14)
+		{
+			n++;
+			_putchar (n + '0');
+		}
+		lines++;
+		_putchar ('\n');
+	}
+	_putchar('\n');
+}
