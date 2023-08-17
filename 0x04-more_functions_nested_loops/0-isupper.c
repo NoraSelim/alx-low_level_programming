e033813150e4d85d636bd6187543628ad82509cc
Norhan Hamdy <norahamdy141@gmail.com>
2023-08-17 02:18:19 -0700

    tasks

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _isupper - Entry point
+ *
+ * @c : 'the program's about writinh alphabitics'
+ *
+ * Return: Always 0 (Success)
+ */
+int _isupper(int c);
+{
+	int j;
+	int i= 'A';
+	while(i <= 'Z')
+	{
+		i++;
+	}
+	if(j == i)
+	{
+		return (1);
+	else
+		return (0);
+	}
+	
+}
