33d4853415872db0145785a1ff6e8ba0e5751470
Norhan Hamdy <norahamdy141@gmail.com>
2023-08-17 03:52:40 -0700

    tasks

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,11 +3,11 @@
  * mul - Entry point
  *
  * @a:'return value'
- * @b: 'return value'
+ * @b:'return value'
  *
  * Return: return multiply (Success)
  */
-int mul(int a, int b);
+int mul(int a, int b)
 {
 int multiply = a * b;
 return (multiply);
