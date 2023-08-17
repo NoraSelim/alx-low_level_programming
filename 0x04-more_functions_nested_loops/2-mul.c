3e852d9d54dac2bf6fbadf39d4b27911fd1c520d
Norhan Hamdy <norahamdy141@gmail.com>
2023-08-17 03:48:04 -0700

    tasks

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * mul - Entry point
+ *
+ * Description: 'the program's about writinh alphabitics'
+ *
+ * Return: Always 0 (Success)
+ */
+int mul(int a, int b);
+{
+int multiply = a * b;
+return (multiply);
+}
+
