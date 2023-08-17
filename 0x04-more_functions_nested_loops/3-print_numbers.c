9cbd1a84aa66758af68f2b455f2785313bd07fda
Norhan Hamdy <norahamdy141@gmail.com>
2023-08-17 04:07:53 -0700

    tasks

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *  - Entry point
+ *
+ * print_numbers: 'print numbers'
+ *
+ * Return: Always 0 (Success)
+ */
+void print_numbers(void);
+{
+	int a;
+	for(a = 0; a <= 9; a++)
+	{
+	_putchar('a');
+	}
+_putchar('\n');
+}
+
