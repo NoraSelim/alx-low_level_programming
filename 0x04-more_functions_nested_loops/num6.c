19c932c4df78c37840bb970c637cb9a2e5566c00
Norhan Hamdy <norahamdy141@gmail.com>
2023-08-17 17:18:33 -0700

    tasks

--- 0x04-more_functions_nested_loops/num6.c
+++ 0x04-more_functions_nested_loops/num6.c
@@ -1,25 +1,28 @@
-#include "main.h"
+#include <stdio.h>
 /**
  *  print_line - Entry point
  *
- * @n: 'drawing a line'
+ * @n: 'the program's about writinh alphabitics'
  *
  * Return: Always 0 (Success)
  */
 void print_line(int n)
 {
+	int n = 3;
 	int line;
 
 	if ( n <= 0 )
 	{
-		_putchar('\n');
+		putchar('\n');
 	}
 	else
 	{
 		for (line = 1; line <= n; line++)
 		{
-			_putchar("_");
+			putchar("_");
+			putchar('\n');
 		}
 	}
-	_putchar('\n');
+
 }
+~   
