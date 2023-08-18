cd0b1f5164b1c9bda81a6f0bf2766935990d6e78
Norhan Hamdy <norahamdy141@gmail.com>
2023-08-17 18:11:38 -0700

    tasks

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -4,20 +4,16 @@
  * print_line - prints _ for n times
  * @n: number of repeats
  */
+
 void print_line(int n)
 {
-	int line;
-
-	if (n <= 0)
+	if (n > 0)
 	{
-		_putchar('\n');
-	}
-	else
-	{
-		for (line = 1; line <= n; line++)
+		while (n)
 		{
-			_putchar("_");
+			putchar('_');
+			n--;
 		}
-		_putchar('\n')
 	}
+	_putchar('\n');
 }
