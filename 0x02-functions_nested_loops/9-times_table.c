8dca618add9812bad63ad38b878e3beae2e55490
Norhan Hamdy <norahamdy141@gmail.com>
2023-08-20 02:36:16 -0700

    tasks

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,13 +8,13 @@
  */
 void times_table(void)
 {
-	int line,x,Y;
-	int line == y ;
+	int line, x, Y;
+	int line == y;
 	int z = x * Y;
 
-	for (line = 0; line < x ; line++)
+	for (line = 0; line < x; line++)
 	{
-		for ( x = 0; x < 8; x++)
+		for (x = 0; x < 8; x++)
 		{
 			Putchar('z');
 			putchar(',');
