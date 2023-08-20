77388080b670c4de817ed4c2c3b729fb3e42b0d1
Norhan Hamdy <norahamdy141@gmail.com>
2023-08-20 02:29:18 -0700

    task

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * times_table - Entry point
+ *
+ * Description: 'time table'
+ *
+ * Return: Always 0 (Success)
+ */
+void times_table(void)
+{
+	int line,x,Y;
+	int line == y ;
+	int z = x * Y;
+
+	for (line = 0; line < x ; line++)
+	{
+		for ( x = 0; x < 8; x++)
+		{
+			Putchar('z');
+			putchar(',');
+			putchar(' ');
+		}
+	_putchar('\n');
+	}
+}
