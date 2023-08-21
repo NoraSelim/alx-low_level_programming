15d95ddf247ca3896d4f643b63a1bbc7328b4214
Norhan Hamdy <norahamdy141@gmail.com>
2023-08-21 09:22:00 -0700

    tasks

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * reset_to_98 - Entry point
+ *
+ * @n: 'the program's about writinh alphabitics'
+ *
+ * Return: Always 0 (Success)
+ */
+void reset_to_98(int *n)
+{
+	n* = 98
+}
+
+
