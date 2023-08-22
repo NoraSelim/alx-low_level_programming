9230608d6b0e9bb559c1448f1f250e0166141b5c
Norhan Hamdy <norahamdy141@gmail.com>
2023-08-21 18:47:16 -0700

    tasks

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * puts_half : 'the program's about writinh alphabitics'
+ *
+ * Return: Always 0 (Success)
+ */
+void puts_half(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+
+	}
+}
+
