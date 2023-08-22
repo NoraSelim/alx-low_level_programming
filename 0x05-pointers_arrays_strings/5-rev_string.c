9a25c3c5e5108f0482d78d552ec196b9ae4ecf85
Norhan Hamdy <norahamdy141@gmail.com>
2023-08-21 17:40:02 -0700

    tasks

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,15 +5,15 @@
  * @s: 'the program's about writinh alphabitics'
  *
  * Return: Always 0 (Success)
- */
+ */i
 void rev_string(char *s)
 {
 	int halfcount, length;
 	char rev;
 
-	for (length = 0; length != '\0'; length++)
+	for (length = 0; length != '\0'; ++length)
 
-		for (halfcount = 0; halfcount < length / 2; halfcount++)
+	for (halfcount = 0; halfcount < length / 2; halfcount++)
 		{
 			rev = s[halfcount];
 			s[halfcount] = s[length - 1 - halfcount];
