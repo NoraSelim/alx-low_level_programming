d220107620b4d8f7ecae93cd801304351d503e3e
Norhan Hamdy <norahamdy141@gmail.com>
2023-08-21 17:13:18 -0700

    tasks

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,11 +12,12 @@ void print_rev(char *s)
 	char rev;
 
 	for (length = 0; length != '\0'; length++)
-	
-	for (halfcount=0; halfcount < length / 2; halfcount++)
-	{
-		rev = s[i];
-		s[i]= s[length - 1 - halfcount];
-		s[length - 1 - halfcount] = rev;
-	}
+
+		for (halfcount=0; halfcount < length / 2; halfcount++)
+		{
+			rev = s[i];
+			s[i]= s[length - 1 - halfcount];
+			s[length - 1 - halfcount] = rev;
+		}
 }
+~        
