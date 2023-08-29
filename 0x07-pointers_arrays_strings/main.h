107ddace33518e32cfa90963bdf422b04a07f256
Norhan Hamdy <norahamdy141@gmail.com>
2023-08-28 19:59:06 -0700

    tasks

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,4 +8,6 @@ char *_strncat(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 int _strcmp(char *s1, char *s2);
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 #endif
