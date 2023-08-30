39c62a41efcd969baf5ba1936a4ca7efbf53b0cb
Norhan Hamdy <norahamdy141@gmail.com>
2023-08-30 16:51:48 -0700

    tasks

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,12 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void _puts_recursion(char *s);
 int _putchar(char c);
-char *_strcat(char *dest, char *src);
-char *_strncpy(char *dest, char *src, int n);
-char *_strncat(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
-int _strcmp(char *s1, char *s2);
+void _puts_recursion(char *s);
 #endif
 ~        
