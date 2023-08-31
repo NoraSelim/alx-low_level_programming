ce152f0fe15d11556c8c51d5344bd026d3b59be2
Norhan Hamdy <norahamdy141@gmail.com>
2023-08-30 18:26:58 -0700

    tasks

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,12 +1,8 @@
 #include "main.h"
 
 /**
- *_print_rev_recursion - prints a string in reverse and adds a newline using
- *recursion
- *
- *@s: pointer that has the address of the string to be printed
- *
- *Return: nothing
+ * _print_rev_recursion - prints a string in reverse and adds a newline using recurssive
+ * @s: pointer that has the address of the string to be printed
  */
 
 void _print_rev_recursion(char *s)
