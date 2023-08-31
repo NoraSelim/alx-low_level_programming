8d08bbf8b35b30d413d6d6edd734eb69881cc135
Norhan Hamdy <norahamdy141@gmail.com>
2023-08-30 17:46:56 -0700

    tasks

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,9 +4,8 @@
  *_puts_recursion - prints a string and adds a newline using recursion
  *
  *@s: pointer that has the address of the string to be printed
- *
- *Return: nothing
  */
+
 void _puts_recursion(char *s)
 {
 	if (*s != '\0')
