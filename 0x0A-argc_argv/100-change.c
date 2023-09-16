5a26d0f85b6e4253def496c612d5952acf06bb9c
Norhan Hamdy <norahamdy141@gmail.com>
2023-09-15 19:38:22 -0700

    tasks

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,40 +1,48 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <stdbool.h>
 
 /**
- * coinConverter - Helper function that does all the mathematics
- * @i: Passed in variable from main for calculations
- * Return: The number of coins needed minimum for the passed in variable
+ * main - Entry point
+ *@argc: number of arguments
+ *@argv: string with arguments
+ * Return: Always 0 (Success)
  */
-int coinConverter(int i)
+int main(int argc, char *argv[])
 {
-	int count = 0;
+	int monedas;
+	int j;
+	int count;
+	int coins[5] = {25, 10, 5, 2, 1};
 
-	while (i != 0)
+	count = 0;
+	monedas = 0;
+
+	if (argc == 2)
 	{
-		if (i % 10 == 9 || i % 10 == 7)
-			i -= 2;
-		else if (i % 25 == 0)
-			i -= 25;
-		else if (i % 10 == 0)
-			i -= 10;
-		else if (i % 5 == 0)
-			i -= 5;
-		else if (i % 2 == 0)
+		for (j = 0; j <= 4; j++)
 		{
-			if (i % 10 == 6)
-				i -= 1;
-			else
-				i -= 2;
+			if (atoi(argv[1]) >= coins[j])
+			{
+				monedas = atoi(argv[1]) / coins[j];
+				count = atoi(argv[1]) % coins[j];
+				break;
+			}
 		}
-		else
-			i -= 1;
 
-		count++;
+		for (j = 0; j <= 4; j++)
+		{
+			if (count >= coins[j])
+			{
+				monedas++;
+				count = count - coins[j];
+				j = 0;
+			}
+		}
+	} else
+	{
+		printf("Error\n");
+		return (1);
 	}
-
-	return (count);
+	printf("%d\n", monedas);
+	return (0);
 }
