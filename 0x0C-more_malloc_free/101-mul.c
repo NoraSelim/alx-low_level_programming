3ddbc8cd000793619ee17f1a0512fecbaf6b02e7
Norhan Hamdy <norahamdy141@gmail.com>
2023-09-15 19:34:45 -0700

    tasks

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,29 +2,29 @@
 #include <stdlib.h>
 #include "main.h"
 /**
-* main - multiply 2 numbers as arguments
-* @argc: number of arguments
-* @argv: arguments
-* Return: 0 success
-*/
+ * main - multiply 2 numbers as arguments
+ * @argc: number of arguments
+ * @argv: arguments
+ * Return: 0 success
+ */
 int main(int argc, char *argv[])
 {
-unsigned long mul;
-int x, j;
+	unsigned long mul;
+	int x, j;
 	if (argc != 3)
 	{ printf("Error\n");
-	exit(98); }
+		exit(98); }
 	for (x = 1; x < argc; x++)
 	{
 		for (j = 0; argv[x][j] != '\0'; j++)
 		{
 			if (argv[x][j] > 57 || argv[x][j] < 48)
 			{  printf("Error\n");
-			exit(98); }
+				exit(98); }
 		}
 
 	}
 	mul = atol(argv[1]) *  atol(argv[2]);
 	printf("%lu\n", mul);
-return (0);
+	return (0);
 }
