047b6801155b98af65cc8b99ab43d5100d322989
Norhan hamdy <norahamdy141@gmail.com>
2023-09-30 21:37:03 -0700

    task 6 adjust

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -16,11 +16,11 @@ int pop_listint(listint_t **head)
 	listint_t *temp;
 	int data = 0;
 
-	if (head == Null && head* == Null)
+	if (head == NULL && head* == NULL)
 		return (0);
 	temp = *head;
 	data = temp->n;
-	head* = (head*)->next;
+	*head = (*head)->next;
 	free(temp);
-	return(data);
+	return (data);
 }
